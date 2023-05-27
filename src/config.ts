d0e460a72e849eb04012be97fd6ff3d382b1b162
Joerg Schoeneburg <schoeneburg.j@googlemail.com>
2023-05-27 16:30:06 +0200

    finished ueber mich

--- src/config.ts
+++ src/config.ts
@@ -30,6 +30,63 @@ const SiteConfig = {
             name: 'AGB',
             href: '/agb'
         }
+    ],
+    skills: [
+        {
+            name: 'CSS',
+            x: '-10vw',
+            y: '-10vw'
+        },
+        {
+            name: 'HTML',
+            x: '-21vw',
+            y: '2vw'
+        },
+        {
+            name: 'JavaScript',
+            x: '-24vw',
+            y: '16vw'
+        },
+        {
+            name: 'Github',
+            x: '-31vw',
+            y: '-5vw'
+        },
+        {
+            name: 'SQL',
+            x: '20vw',
+            y: '6vw'
+        },
+        {
+            name: 'React',
+            x: '0vw',
+            y: '12vw'
+        },
+        {
+            name: 'Tailwind',
+            x: '23vw',
+            y: '15vw'
+        },
+        {
+            name: 'Figma',
+            x: '10vw',
+            y: '-4vw'
+        },
+        {
+            name: 'PHP',
+            x: '12vw',
+            y: '-16vw'
+        },
+        {
+            name: 'Web Design',
+            x: '28vw',
+            y: '-10vw'
+        },
+        {
+            name: 'CMS',
+            x: '-12vw',
+            y: '-21vw'
+        }
     ]
 };
 
