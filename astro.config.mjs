19be719b5654f8cb0c0e54bb6229735128092608
Joerg Schoeneburg <schoeneburg.j@googlemail.com>
2023-07-31 15:41:30 +0200

    site options added

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,23 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import prefetch from '@astrojs/prefetch';
 import image from '@astrojs/image';
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx';
+
+const sites = [
+    'https://master.dpoxj8hd9osr0.amplifyapp.com/',
+    'https://portfolio.joerg-schoeneburg.de/'
+];
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap(), tailwind(), prefetch(), image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  }), mdx()],
-  site: 'https://master.dpoxj8hd9osr0.amplifyapp.com/'
+    integrations: [
+        sitemap(),
+        tailwind(),
+        prefetch(),
+        image({
+            serviceEntryPoint: '@astrojs/image/sharp'
+        }),
+        mdx()
+    ],
+    site: sites[1]
 });
