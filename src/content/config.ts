0e4e1810c8407a687d4f06fee9116723034f67ac
Joerg Schoeneburg <schoeneburg.j@googlemail.com>
2023-08-31 20:13:45 +0200

    fixed image processing

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,16 +2,15 @@ import { defineCollection, z } from 'astro:content';
 
 const projectsCollection = defineCollection({
     type: 'content',
-    schema: z.object({
-        title: z.string(),
-        description: z.string(),
-        tags: z.array(z.string()),
-        image: z.object({
-            src: z.string(),
-            alt: z.string()
-        }),
-        isFeatured: z.boolean()
-    })
+    schema: ({ image }) =>
+        z.object({
+            title: z.string(),
+            description: z.string(),
+            tags: z.array(z.string()),
+            image: image(),
+            image_alt: z.string(),
+            isFeatured: z.boolean()
+        })
 });
 
 export const collections = {
--- src/content/projects/project-2.md
+++ src/content/projects/project-2.md
@@ -2,7 +2,8 @@
 title: 'Lore Ipsum'
 description: 'Lorem ipsum dolor sit amet consecte tur adipiscing elit. Aliquam atque ipsam nihil cumque.'
 tags: [development, webtemplate]
-image: { src: '/demoprojekt.jpg', alt: 'demoprojekt' }
+image: './images/demoprojekt.jpg'
+image_alt: 'demoprojekt'
 isFeatured: false
 slug: 'demoprojekt-2'
 ---
