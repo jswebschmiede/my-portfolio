3001f0d49e84d93cec2c9d1df77993218d8f653a
Joerg Schoeneburg <schoeneburg.j@googlemail.com>
2023-09-06 12:59:47 +0200

    refactor code

---
+++ src/scripts/components/navLinkManager.ts
@@ -0,0 +1,33 @@
+class NavLinkManager {
+  private element: HTMLElement;
+  private navLinks: HTMLElement[];
+
+  constructor(element: HTMLElement) {
+    this.element = element;
+    this.navLinks = Array.from(this.element.querySelectorAll("a"));
+
+    this.updateNavLinks();
+  }
+
+  private updateNavLinks() {
+    const path = window.location.pathname;
+
+    this.navLinks.forEach((link) => {
+      const href = link.getAttribute("href");
+      const span = link.children[0] as HTMLElement | null;
+
+      if (span) {
+        if (href === path) {
+          link.setAttribute("aria-current", "page");
+          span.classList.add("w-5", "bg-black-800");
+        } else {
+          link.removeAttribute("aria-current");
+          span.classList.remove("w-5", "bg-black-800");
+          span.classList.add("w-0", "bg-black-200");
+        }
+      }
+    });
+  }
+}
+
+export { NavLinkManager };
