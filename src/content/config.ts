c98903a2385418923899c696e5544b92d9777b14
Joerg Schoeneburg <schoeneburg.j@googlemail.com>
2024-06-18 13:27:27 +0200

    added new projects and sort per date

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,20 @@
-import { defineCollection, z } from 'astro:content';
+import { date } from "astro/zod";
+import { defineCollection, z } from "astro:content";
 
 const projectsCollection = defineCollection({
-    type: 'content',
-    schema: ({ image }) =>
-        z.object({
-            title: z.string(),
-            description: z.string(),
-            tags: z.array(z.string()),
-            image: image(),
-            image_alt: z.string(),
-            isFeatured: z.boolean()
-        })
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      tags: z.array(z.string()),
+      image: image(),
+      image_alt: z.string(),
+      isFeatured: z.boolean(),
+      date: z.string(),
+    }),
 });
 
 export const collections = {
-    projects: projectsCollection
+  projects: projectsCollection,
 };
