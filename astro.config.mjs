7ca66fad74ed5cbe7bcb053c97fecf9085ea461e
Joerg Schoeneburg <schoeneburg.j@googlemail.com>
2024-12-04 00:16:35 +0100

    migrate astro prefetch and update navigation components to use data-astro-prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig, sharpImageService } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import prefetch from "@astrojs/prefetch";
 import mdx from "@astrojs/mdx";
 
 const sites = ["https://master.dpoxj8hd9osr0.amplifyapp.com/", "https://joerg-schoeneburg.de/"];
@@ -11,6 +10,7 @@ export default defineConfig({
   image: {
     service: sharpImageService(),
   },
-  integrations: [sitemap(), tailwind({ applyBaseStyles: false }), prefetch(), mdx()],
+  integrations: [sitemap(), tailwind({ applyBaseStyles: false }), mdx()],
   site: sites[1],
+  prefetch: true,
 });
