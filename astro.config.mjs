3431d733de6430ee39793733beccd141146e4b6c
jswebschmiede <schoeneburg.j@googlemail.com>
2026-02-06 17:53:52 +0100

    build: add static output configuration to Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,11 @@ const sites = ["https://master.dpoxj8hd9osr0.amplifyapp.com/", "https://joerg-sc
 
 // https://astro.build/config
 export default defineConfig({
-  image: {
-    service: sharpImageService(),
-  },
-  integrations: [sitemap(), tailwind({ applyBaseStyles: false }), mdx()],
-  site: sites[1],
-  prefetch: true,
+    image: {
+        service: sharpImageService(),
+    },
+    integrations: [sitemap(), tailwind({ applyBaseStyles: false }), mdx()],
+    site: sites[1],
+    prefetch: true,
+    output: "static",
 });
